f23d6c4cac737b4ff7ab68f0568aae045acf67c5
bmalbright <brianalbright@utexas.edu>
2021-09-29 23:22:02 -0500

    submitting homework

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # homework-4
+
+Our task was to build a quiz with javascript that incorporates a timer for answering questions, keeps score, and allows input for a high score table. 
+
+I got a timer from a reference in Slack, but I never got it to work, so it's commented out in the script. 
+
+I set up questions, possible answers, and correct answers in an array. 
+
+The questions I made up off the top of my head, and the answers are accurate. 
+
+The html and css is a modified version of the code from the homework 3. 
+
+There were several students working in zoom groups I hosted that were working on and sharing code, but I don't think I copied anything from them. Any similaririties would be incidental. 
+
+I worked with Ethan Cho extensively on what code I have. I never saw his, so any similarities are from his tutoring me. It was at his advice I stopped working on this project to go back and drill some basics while turning in what I have, which is maybe the best advice I've gotten since bootcamp started. 
+
+
+
+![](homework-4.png)
+
--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,5 @@
 
-startBtn.addEventListener("click") function(currentQuestion) {
-
-
-    
+/*startBtn.addEventListener("click", function() {
     var timer = 15;
     var interval = setInterval(function () {
         document.getElementById('count').innerHTML = timer;
@@ -12,12 +9,22 @@ startBtn.addEventListener("click") function(currentQuestion) {
             document.getElementById('count').innerHTML = "You're out of time!";
         }
     }, 1000);
+})*/
 
-    function currentQuestion(questions) {
-        questions [];
-}
+function currentQuestion() {
+    questions [0].questionOne;
+    var questionsDiv = document.getElementById('questions');
+    questionsDiv.textContent = questions [0].questionOne;
+    for (var i = 0; i < 4; i++) {
+        var answerBtn = document.createElement("button");
+        answerBtn.textContent = questions[0].answerChoices[i];
+        questionsDiv.append(answerBtn);
+    }
 }
 
+function rightAnswer () {
+    
+}
 
 var score
 var highScores
@@ -26,28 +33,33 @@ var questions = [{
     questionOne: "Whose world record did Michael Phelps not break at the 2008 Olympics when he won 8 gold medals?",
     answerChoices: ["Aaron Piersol", "Ian Crocker", "Ryan Lochte", "Caeleb Dressel"],
     correctAnswer: "Ian Crocker"
-};
+},
 
 {
-    questionOne: "Who is the first swimmer to qualify for four Olympic games?",
+    question: "Who is the first swimmer to qualify for four Olympic games?",
         answerChoices: ["Dara Torres", "Katie Ledecky", "Janet Evans", "Jill Sterkel"],
             correctAnswer: "Jill Sterkel"
-};
+},
 
 {
     questionOne: "Who beat Matt Biondi in the 1988 Olympics to end his chance of winning seven gold medals?",
         answerChoices: ["Tom Jaeger", "Rowdy Gaines", "Mark Spitz", "Anthony Nesty"],
             correctAnswer: "Anthony Nesty"
-};
+},
 
 {
     questionOne: "How many Olympic Medals has Michael Phelps won?",
         answerChoices: ["23", "25", "28", "31"],
             correctAnswer: "28"
-};
+},
 
 {
     questionOne: "Who is considered the greatest womens swimmer of all time?",
         answerChoices: ["Katie Ledecky", "Janet Evans", "Dara Torres", "Allison Schmitt"],
             correctAnswer: "so"
-};
+}]
+
+
+console.log(questions[0]);
+console.log(questions [0].questionOne);
+currentQuestion();
